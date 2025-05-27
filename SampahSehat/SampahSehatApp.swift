abe0cd080946be45af5c4e179216407c6a71970f
Matthew Regan Hadiwidjaja <mhadiwidjaja@student.ciputra.ac.id>
2025-05-27 15:42:26 +0700

    Initial Commit

---
+++ SampahSehat/SampahSehatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampahSehatApp.swift
+//  SampahSehat
+//
+//  Created by student on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SampahSehatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
